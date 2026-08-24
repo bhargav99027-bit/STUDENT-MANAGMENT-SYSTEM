42538f2c9b70ab434c5ad45b24f58fcdd5ec93ba
Bhargav hv <bhargav99027@gmail.com>
2026-08-24 14:32:09 +0530

    Add switch-case menu handling

--- main.cpp
+++ main.cpp
@@ -71,5 +71,92 @@ int main()
     cout<<"7--> LOAD STUDENT DATA"<<endl;
     cout<<"8--> DISPLAY MENU AGAIN"<<endl;
     cout<<"0--> EXIT"<<endl;
+     while(1)
+    {
+    int  ch;
+    cout<<"---------------------------------------------------------------------------------------------"<<endl;
+    cout<<"ENTER YOUR CHOICE: ";
+    cin>>ch;
+    switch (ch)
+    {
+    case 1:
+    {
+        Student student1;
+        student1.getInfo();
+        s.push_back(student1);
+
+        fstream file("document.txt", ios::out | ios::app);
+        if (file.is_open()) 
+        {
+           student1.saveToFile(file);
+           cout << "Student data saved successfully!" << endl;
+        } 
+        else 
+        {
+           cout << "Error opening file." << endl;
+        }
+        file.close();
+        break;
+    }
+    case 2:
+    {
+       display_students();
+       break;
+    }
+    case 3:
+    {
+        search_student();
+        break;
+    }
+    case 4:
+    {
+        update_student();
+        break;
+    }
+    case 5:
+    {
+        delete_student();
+        break;
+    }
+    case 6:
+    {
+        save_student();
+        break;
+    }
+    case 7:
+    {
+        load_student();
+        break;
+    }
+    case 8:
+    {
+      cout<<"--------------------------------------------------------------------------------------------"<<endl;
+      cout<<"===============================STUDENT MANAGMENT SYSTEM====================================="<<endl;
+      cout<<"--------------------------------------------------------------------------------------------"<<endl;
+      cout<<"1--> ADD STUDENT"<<endl;
+      cout<<"2--> DISPLAY STUDENTS"<<endl;
+      cout<<"3--> SEARCHING STUDENT"<<endl;
+      cout<<"4--> UPDATE STUDENT"<<endl;
+      cout<<"5--> DELETE STUDENT"<<endl;
+      cout<<"6--> SAVE STUDENT DATA"<<endl;
+      cout<<"7--> LOAD STUDENT DATA"<<endl;
+      cout<<"8--> DISPLAY MENU AGAIN"<<endl;
+      cout<<"0--> EXIT"<<endl;  
+        break;
+    }
+    case 0:
+    {
+        cout<<"EXITED FROM MANAGMENT"<<endl;
+        return 0;
+    }
+    
+    default:
+    {
+        cout<<"INVALID STATE, PLEASE TRY WHICH ARE IN MENU"<<endl;
+        break;
+    }
+  }
+
+ }
 }
 
