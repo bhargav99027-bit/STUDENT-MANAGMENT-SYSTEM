5b8f2520da1906b1fcbfd344d2cc3337f3604dd6
Bhargav hv <bhargav99027@gmail.com>
2026-08-24 14:09:06 +0530

    Set up project structure

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+void load_student()
+{
+    cout<<"DEVELOPMENT UNDER PROCESS"<<endl;
+}
+
+void save_student()
+{
+     cout<<"DEVELOPMENT UNDER PROCESS"<<endl;
+}
+
+void delete_student()
+{
+     cout<<"DEVELOPMENT UNDER PROCESS"<<endl;
+}
+
+void update_student()
+{
+     cout<<"DEVELOPMENT UNDER PROCESS"<<endl;
+}
+
+void display_students()
+{
+    cout<<"DEVELOPMENT UNDER PROCESS"<<endl;
+}
+
+void search_student()
+{
+  cout<<"DEVELOPMENT UNDER PROCESS"<<endl;
+}
+
+class Student {
+private:
+    string usn;
+    string name;
+    string dep;
+    string dob;
+    int age;
+    int sem;
+    float CGPA;
+
+public:
+    void getInfo() 
+    {
+        cout<<"DEVELOPMENT UNDER PROCESS"<<endl;
+    }
+
+    void saveToFile(fstream &file) 
+    {
+         cout<<"DEVELOPMENT UNDER PROCESS"<<endl;
+    }
+};
+
+int main() 
+{
+    vector<Student> s;
+    
+}
+
